300b135df88df5d0787f7a8c3a3ba3408cd7038b
VictorHachard <nicolashachard@gmail.com>
2021-06-22 14:43:13 +0200

    Add new css themes and fix style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+// noinspection JSIgnoredPromiseFromCall
+
+import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../_services/authentication.service";
 import {Router} from "@angular/router";
 import {UserSecurity} from "../_models/user.security";
@@ -8,19 +10,28 @@ import {UserSecurity} from "../_models/user.security";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'user-application';
   currentUser!: UserSecurity;
   value!: string;
 
   constructor(private router: Router,
     private authenticationService: AuthenticationService) {
-    this.authenticationService.currentUser.subscribe(x => { this.currentUser = x; this.value = this.currentUser.themeSimplifiedDto!.name!.replace(' ', '-').toLowerCase();});
+    this.authenticationService.currentUser.subscribe(x => { this.currentUser = x; this.ngOnInit(); });
+  }
+
+  ngOnInit(): void {
+    if (this.currentUser) {
+      this.value = this.currentUser.themeSimplifiedDto!.name!.replace(' ', '-').toLowerCase();
+    } else {
+      this.value = 'default-light';
+    }
   }
 
   logout() {
     this.authenticationService.logout();
     this.router.navigate(['/login']);
+    this.ngOnInit();
   }
 
 }
